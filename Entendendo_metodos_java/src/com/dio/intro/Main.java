e16a3eb7a4607810c08e44396c03e663c4501693
LeviQ27 <leviqueiroz647@gmail.com>
2022-03-08 20:55:28 -0300

    Added Java classes and Main and create Calculador

---
+++ Entendendo_metodos_java/src/com/dio/intro/Main.java
@@ -0,0 +1,22 @@
+package com.dio.intro;
+
+/**
+ * Classe principal dos exercícios da Aula 1de Métodos
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+
+        // Calculadora
+        System.out.println("Exercício da Calculadora");
+        Calculadora.soma(3, 6);
+        Calculadora.subtracao(9, 1.8);
+        Calculadora.multiplicacao(7, 8);
+        Calculadora.divisao(5, 2.5);
+
+
+    }
+    
+}
