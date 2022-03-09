33a8499adf7e998b0b7d51b065124b94ab7f1082
LeviQ27 <leviqueiroz647@gmail.com>
2022-03-08 21:58:51 -0300

    Created Main and Quadrilatero class in sobrecarga

---
+++ Entendendo_metodos_java/src/com/dio/sobrecarga/Main.java
@@ -0,0 +1,18 @@
+package com.dio.sobrecarga;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        
+
+        // Quadrilátero
+        System.out.println("Exercício do Quadrilátero");
+        Quadrilatero.area(8);
+        Quadrilatero.area(7d, 7d);
+        Quadrilatero.area(7, 8, 20);
+        Quadrilatero.area(5f, 6f);
+
+
+    }
+
+}
