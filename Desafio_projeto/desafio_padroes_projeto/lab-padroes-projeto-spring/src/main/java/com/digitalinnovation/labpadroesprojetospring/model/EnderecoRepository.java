5876aa6adc3090235f90142a6893bdb6abd24c76
LeviQ27 <leviqueiroz647@gmail.com>
2022-04-03 08:32:35 -0300

    Added lab-padroes-projeto-spring and conclude Desfio de Projeto 4 - Explorando Padrões de Projetos na Prática Java

---
+++ Desafio_projeto/desafio_padroes_projeto/lab-padroes-projeto-spring/src/main/java/com/digitalinnovation/labpadroesprojetospring/model/EnderecoRepository.java
@@ -0,0 +1,8 @@
+package com.digitalinnovation.labpadroesprojetospring.model;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface EnderecoRepository extends CrudRepository<Endereco, String> 
+{
+    
+}
